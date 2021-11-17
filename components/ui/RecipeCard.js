bb670744a1435512a8f5e3370d3d3a49fefe897f
Jethro Williams <jethrowilliams@outlook.com>
2021-11-17 17:19:00 +0000

    Add links to recipe cards and styling tweaks

--- components/ui/RecipeCard.js
+++ components/ui/RecipeCard.js
@@ -1,21 +1,27 @@
+import Link from 'next/link';
+
 import Flexbox from '../styles/Flexbox';
 import classes from './RecipeCard.module.css';
 
 const RecipeCard = props => {
   return (
     <div className={classes['recipe-card']}>
-      <Flexbox column className={`${classes['content-container']} ${props.className}`}>
-        <div className={classes['photo-container']}>
-          <img src={props.recipe.photo} />
-        </div>
-        <Flexbox column justifyAround className={classes['info-container']}>
-          <h3>{props.recipe.name}</h3>
-          <Flexbox justifyBetween w100>
-            <div>By {props.recipe.author}</div>
-            <div>{`${props.recipe.timeMinutes} minutes`}</div>
+      <Link href={`/recipes/${props.recipe.id}`} passHref>
+        <a>
+          <Flexbox column className={`${classes['content-container']} ${props.className}`}>
+            <div className={classes['photo-container']}>
+              <img src={props.recipe.photo} />
+            </div>
+            <Flexbox column justifyAround className={classes['info-container']}>
+              <h3>{props.recipe.name}</h3>
+              <Flexbox justifyBetween w100>
+                <div>By {props.recipe.author}</div>
+                <div>{`${props.recipe.timeMinutes} minutes`}</div>
+              </Flexbox>
+            </Flexbox>
           </Flexbox>
-        </Flexbox>
-      </Flexbox>
+        </a>
+      </Link>
     </div>
   );
 };
