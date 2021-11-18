baf560c6b167a591b00485a8b2a9bad823b99c1e
Jethro Williams <jethrowilliams@outlook.com>
2021-11-18 17:57:37 +0000

    Move over more potential duplicate code

--- components/ui/recipe-lists/HorizontalRecipeListContainer.js
+++ components/ui/recipe-lists/HorizontalRecipeListContainer.js
@@ -1,31 +1,48 @@
+import { useRef } from 'react';
+
+import Flexbox from '../../styles/Flexbox';
 import Pointer from '../Pointer';
 import classes from './HorizontalRecipeListContainer.module.css';
 
 const pointerSize = 21;
 
 const HorizontalRecipeListContainer = props => {
+  const listContainerRef = useRef();
+
+  const scrollLeftHandler = () => {
+    listContainerRef.current.scrollLeft =
+      listContainerRef.current.scrollLeft - props.cardContainerWidth;
+  };
+
+  const scrollRightHandler = () => {
+    listContainerRef.current.scrollLeft =
+      listContainerRef.current.scrollLeft + props.cardContainerWidth;
+  };
+
   const pointerTop = () => {
     // prettier-ignore
-    return props.listHeight / 2 - pointerSize / 2 - 9 / 2;
+    return (props.listHeight / 2) - (pointerSize / 2) - (9 / 2);
   };
 
   return (
     <div className={classes.container}>
       <Pointer
-        onClick={props.leftPointerClickHandler}
+        onClick={scrollLeftHandler}
         direction='left'
         size={pointerSize}
         className={classes['left-pointer']}
         style={{ top: `${pointerTop()}px` }}
       />
       <Pointer
-        onClick={props.rightPointerClickHandler}
+        onClick={scrollRightHandler}
         direction='right'
         size={pointerSize}
         className={classes['right-pointer']}
         style={{ top: `${pointerTop()}px` }}
       />
-      {props.children}
+      <Flexbox className={classes['horizontal-recipe-list']} refName={listContainerRef}>
+        {props.children}
+      </Flexbox>
     </div>
   );
 };
