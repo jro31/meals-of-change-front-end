e716a41d9041dd13030fbed1b104cd32ab2791df
Jethro Williams <jethrowilliams@outlook.com>
2021-12-14 10:46:45 +0000

    Create use-checksum hook

---
+++ hooks/use-checksum.js
@@ -0,0 +1,25 @@
+import CryptoJS from 'crypto-js';
+
+const useChecksum = async file => {
+  const md5FromFile = () => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = fileEvent => {
+        const binary = CryptoJS.lib.WordArray.create(fileEvent.target.result);
+        const md5 = CryptoJS.MD5(binary);
+        resolve(md5);
+      };
+      reader.onerror = () => {
+        reject('Something went wrong with the file reader');
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const md5 = await md5FromFile();
+  const checksum = md5.toString(CryptoJS.enc.Base64);
+  return checksum;
+};
+
+export default useChecksum;
