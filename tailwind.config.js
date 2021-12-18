ea961b916f03ffee34c7b8ad8fdb1e73c54fbeac
Jethro Williams <jethrowilliams@outlook.com>
2021-12-18 13:17:58 +0000

    Tweak code for Tailwind V3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       animation: {
@@ -64,11 +63,6 @@ module.exports = {
       width: {
         108: '27rem',
       },
-    },
-  },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
     },
   },
   plugins: [],
