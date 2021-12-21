8123c1f8abe820f61610514f9c8604dcfb68eb59
Jethro Williams <jethrowilliams@outlook.com>
2021-12-21 17:59:20 +0000

    Conflict resolution

--- components/layout/navbar/invasive-components/MainMenu.js
+++ components/layout/navbar/invasive-components/MainMenu.js
@@ -19,7 +19,37 @@ const MainMenu = props => {
       transitionClassNames={transitionClassNames}
     >
       <div className='flex fixed top-14 left-0 bg-white z-30 p-10 h-screen-minus-nav md:h-auto md:min-h-33 min-w-75 md:w-full'>
-        <div>Recipes</div>
+        <div className='flex-initial pr-16'>
+          <div onClick={() => navigateTo('/')} className='font-bold cursor-pointer'>
+            Home
+          </div>
+        </div>
+        <div className='flex flex-col flex-grow-only px-16'>
+          <div
+            onClick={() => navigateTo('/recipes')}
+            className='flex-initial pb-2 font-bold cursor-pointer'
+          >
+            Recipes
+          </div>
+          <div className='flex-grow-only columns-2xs pt-2'>
+            {tags.map(tag => {
+              return (
+                <div
+                  key={tag}
+                  onClick={() => navigateTo(`/recipes?tag_name=${tag.toLowerCase()}`)}
+                  className='cursor-pointer'
+                >
+                  {tag}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className='flex-initial pr-16'>
+          <div onClick={addRecipeClickHandler} className='font-bold cursor-pointer'>
+            Add a recipe
+          </div>
+        </div>
       </div>
     </Container>
   );
