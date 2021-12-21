bf56ae61aade1411787622116973f6cb5a46fcc3
Jethro Williams <jethrowilliams@outlook.com>
2021-12-21 12:31:54 +0000

    Call the tags API on app-load, and set to state

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,14 @@
-import { Fragment, useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { loginStatusActions } from '../../store/login-status';
+import { mainMenuActions } from '../../store/main-menu';
 import Navbar from './navbar/Navbar';
 
 const Layout = props => {
   const dispatch = useDispatch();
 
-  const checkLoginStatus = async () => {
+  const checkLoginStatus = useCallback(async () => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/logged_in`, {
       credentials: 'include',
     });
@@ -16,11 +17,28 @@ const Layout = props => {
     if (data.logged_in) {
       dispatch(loginStatusActions.login());
     }
-  };
+  }, [dispatch]);
+
+  const loadTags = useCallback(async () => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/tags?limit=10`, {});
+
+      if (!response.ok) {
+        throw new Error('Unable to fetch tags');
+      }
+
+      const data = await response.json();
+
+      dispatch(mainMenuActions.setTags(data.tags));
+    } catch (error) {
+      // TODO - Handle this
+    }
+  }, [dispatch]);
 
   useEffect(() => {
     checkLoginStatus();
-  }, []);
+    loadTags();
+  }, [checkLoginStatus, loadTags]);
 
   return (
     // Put any app-wide styling in this div
